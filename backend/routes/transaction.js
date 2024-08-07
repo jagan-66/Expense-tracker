2656d64503f7dbdf156420e024ca3af96dd0b4d2
jagan-66 <jagans037@gmail.com>
2024-08-07 12:36:26 +0530

    POST server working

--- backend/routes/transaction.js
+++ backend/routes/transaction.js
@@ -1,7 +1,9 @@
+const { addIncome } = require("../controllers/Income");
+
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello World");
-});
+
+router.post('/add-income', addIncome) 
+
 
 module.exports = router;
